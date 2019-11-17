ab5d73033f7ed1ebcc0dd6d1ebbed8772f446ec6
tien238lnd <39857648+tien238lnd@users.noreply.github.com>
2019-11-17 20:50:39 +0700

    Add: Read label from csv file

--- Read_dataset.py
+++ Read_dataset.py
@@ -8,12 +8,22 @@ Created on Sun Nov 17 20:01:31 2019
 
 import numpy as np
 import scipy.io.wavfile as wav
+import csv
 
 # Mỗi file đọc ra được 1 mảng numpy 1D size khoảng 30000~50000.
 # Vì các row có size khác nhau nên ko đưa vô mảng numpy2D được, trước mắt anh để chung trong 1 list đặt tên data như này đã
 data = []   
+label = []
 
-data_dir = "Handout/Train/PAEP-"
-for i in range(1, 5230):
-    (rate, signal) = wav.read(data_dir + str(i).zfill(6) + ".wav")
-    data.append(signal)
+data_dir = "Handout/Train/"
+
+with open('Handout/train_label.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader)    # bỏ qua dòng đầu tiên của file csv
+    for row in csv_reader:
+        rate, signal = wav.read(data_dir + row[0])
+        data.append(signal)
+        label.append(int(row[1]))
+    
+        
+# Feature engineering
